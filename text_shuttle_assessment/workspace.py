60cf193b50892fb1914de41cdbd794f3bf947114
Joao4569 <johnnyrod4569@gmail.com>
2023-04-05 13:30:28 +0000

    done

---
+++ text_shuttle_assessment/workspace.py
@@ -0,0 +1,4 @@
+from function_library import strip_skintone_mod, extract_skintone_modifiers
+
+strip_skintone_mod("dashkj hfdshf emoji h dfahfhfoish")
+extract_skintone_modifiers("red human emoji blue pink yellow human emoji green")
