17bb1f3470716d395c03288b69329b8612289b9c
Joao4569 <johnnyrod4569@gmail.com>
2023-04-05 13:58:34 +0000

    working through assessment

--- text_shuttle_assessment/function_library.py
+++ text_shuttle_assessment/function_library.py
@@ -1,25 +1,15 @@
 def strip_skintone_mod(input_array):
     if "emoji" in input_array:
-        print("emoji")
         return "emoji"
     else:
         return ""
 
 
 def extract_skintone_modifiers(input_array):
-    if "emoji" in input_array:
-        substrings = input_array.split()
-        sorted_substrings = []
+    substrings = input_array.split()
+    print(substrings)
+    sorted_substrings = []
         
-        for substring in substrings:
-
-            if substring == "human":
-                index_emoji = substrings.index(substring) + 1
-                index_skintone_mod = substrings.index(substring) + 2
-
-                if substrings[index_emoji] == "emoji":
-                    sorted_substrings.append(
-                        f'human emoji : {substrings[index_skintone_mod]}')
-            continue
-        print(sorted_substrings)
+    for substring in substrings:
+        if substring
         
